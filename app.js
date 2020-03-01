c2d9a37c0f74f9c71cc098468e0ec46b8389bb54
whytepeter <peteremmanuelwhyte@gmail.com>
2020-03-01 23:27:51 +0100

    just added type writing effect

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ hamburger.addEventListener("click", () => {
 let formInput = Array.from(document.querySelectorAll(".form-inputs"));
 let labelEmail = document.querySelector(".label-email");
 let labelMessage = document.querySelector(".label-message");
+let submit = document.querySelector(".form");
 
 formInput.forEach(input => {
   input.addEventListener("focus", e => {
@@ -40,8 +41,73 @@ formInput.forEach(input => {
 });
 
 // Form validation
-let submitBnt = document.querySelector(".submit");
 
-submitBnt.addEventListener("submit", e => {
-  e.preventdefault();
-});
+const patterns = /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/;
+
+// Type Writing script
+
+const TypeWriter = function(txtElement, words, wait = 3000) {
+  this.txtElement = txtElement;
+  this.words = words;
+  this.txt = "";
+  this.wordIndex = 0;
+  this.wait = parseInt(wait, 10);
+  this.type();
+  this.isDeleting = false;
+};
+
+// Type Method
+TypeWriter.prototype.type = function() {
+  // Current index of word
+  const current = this.wordIndex % this.words.length;
+  //Get full text of current word
+  const fullTxt = this.words[current];
+
+  //Check if deleting
+  if (this.isDeleting) {
+    //Remove char
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+  } else {
+    // Add char
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
+  }
+
+  // Insert txt into element
+  this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`;
+
+  // Initial Type Speed
+  let typeSpeed = 300;
+  if (this.isDeleting) {
+    typeSpeed /= 2;
+  }
+
+  //Check if word is complete
+  if (!this.isDeleting && this.txt === fullTxt) {
+    //Make a pause at end
+    typeSpeed = this.wait;
+    //Set delet to true
+    this.isDeleting = true;
+  } else if (this.isDeleting && this.txt === "") {
+    this.isDeleting = false;
+    //Move to the next word
+    this.wordIndex++;
+    // Pasue before start typing
+    typeSpeed = 500;
+  }
+
+  console.log(fullTxt);
+  setTimeout(() => this.type(), typeSpeed);
+};
+
+// Init On DOM Load
+document.addEventListener("DOMContentLoaded", init);
+
+//Init App
+function init() {
+  const txtElement = document.querySelector(".website");
+  const words = JSON.parse(txtElement.getAttribute("data-words"));
+  const wait = txtElement.getAttribute("data-wait");
+  // Init TypeWritter
+
+  new TypeWriter(txtElement, words, wait);
+}
